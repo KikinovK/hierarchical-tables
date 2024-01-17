153d8df47384979e2179f49cd22ac6c84ef3d9b8
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-17 20:12:45 +0200

    feat: add config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from 'react-bootstrap';
+
+const App = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h1>Tables</h1>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default App;
