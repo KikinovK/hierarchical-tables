fe6c9d97c4ec3afc0607896db190c4496c484fdc
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-23 22:57:22 +0200

    fix: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: 'hierarchical-tables',
+  base: isProduction ? 'hierarchical-tables' : '',
 });
