35357ab98d31324c085535f389c6cff2ee54ea39
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-23 20:28:10 +0200

    feat add footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,15 +1,17 @@
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 const Layout = () => (
   <>
     <Header />
-    <main className="flex-grow-1">
+    <main className="flex-grow-1 pt-3 pb-3">
       <Container>
         <Outlet />
       </Container>
     </main>
+    <Footer />
   </>
 );
 
